3e58a12dca79c8f46a9776b686c33a8806db89d8
grro <>
2023-01-02 16:58:56 +0100

    logoutput improved

--- pvpower/weather_forecast.py
+++ pvpower/weather_forecast.py
@@ -26,7 +26,6 @@ class WeatherForecast:
         offset_hour = round((datetime.now() - datetime.utcnow()).total_seconds() / (60 * 60))
         return datetime.strptime((self.time_utc + timedelta(hours=offset_hour)).strftime("%d.%m.%Y %H:%M:%S"), "%d.%m.%Y %H:%M:%S")
 
-
     def with_time(self, dt: datetime):
         return WeatherForecast(dt,
                                self.irradiance,
